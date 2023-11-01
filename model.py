b237bb8ecc0d1d2daed4674f5718e978619cceb2
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2023-11-01 20:02:50 +0300

    Trained model using simple neural network.

---
+++ model.py
@@ -0,0 +1,44 @@
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+# Load the data
+df = pd.read_csv('Twitter_Data.csv').dropna(subset=['Metinler', 'Duygular'])
+
+X = df['Metinler'].values
+y = df['Duygular'].values
+
+# Tokenize the text data
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(X)
+X = tokenizer.texts_to_sequences(X)
+X = pad_sequences(X, maxlen=100)  # Set a fixed sequence length
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+# Build the model
+model = Sequential([
+    Embedding(input_dim=len(tokenizer.word_index) +
+              1, output_dim=100, input_length=100),
+    LSTM(64),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, batch_size=32,
+          validation_data=(X_test, y_test))
+
+# Evaluate the model
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print(f'Test accuracy: {test_accuracy}')
+
+# After training is complete, save the model
+model.save('sentiment_analysis_model2.h5')

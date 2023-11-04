d2402fb1c9d375b31a23505fd87631730f35fab2
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2023-11-04 19:40:54 +0300

    Switched to categorical cross entropy and softmax function.

--- input.py
+++ input.py
@@ -1,36 +1,32 @@
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
-from model import model
+from keras.models import load_model
+
+# Assuming you saved your model as 'model_CCE.h5'
+model = load_model('model_v3.h5')
 
 # Define an example input
-example_input = "I hope everyone a wonderful day! :) #positive #smile #happy #friends #fun #laugh #play #goodtimes #goodvibes #goodday #lovely #beautiful #smile #fun #friends #love #happy #goodtimes #goodvibes #goodday #lovely #beautiful"
+example_input = ["I hate you! Get lost! You are disgusting!"]
 
 # Tokenization
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(example_input)
 
-# Assuming 'model' is your trained neural network
-
-# Preprocess the input (tokenize, vectorize, etc.)
-# This step depends on how you preprocessed your training data
-# For example, using the tokenizer from the previous example:
+# Preprocess the input (tokenize, etc.)
 tokenized_input = tokenizer.texts_to_sequences(example_input)
 padded_sequence = pad_sequences(
     tokenized_input, maxlen=100, padding='post', truncating='post')
-# input_vector = tokenizer.sequences_to_matrix(padded_sequence, mode='binary')
 
 # Make a prediction
 predicted_output = model.predict(padded_sequence)
 
-print("Value: ", predicted_output[0])
+print("Value: ", predicted_output)
 
-if predicted_output[0] > 0.5:
-    print("Positive sentiment")
-else:
-    print("Negative sentiment")
+# Get the index of the highest probability
+predicted_class = predicted_output.argmax()
 
-# The predicted_output will depend on the specific task and model architecture
-# For example, in sentiment analysis, it might be a probability of positive/negative sentiment
+# Define a list of sentiment labels corresponding to the classes
+sentiment_labels = ['Pozitif', 'Nötr', 'Negatif']
 
-# If needed, you can interpret the predicted output based on your task
-# For example, in sentiment analysis, you might check if the probability is above a threshold for a positive sentiment
+# Print out the predicted sentiment
+print(f'Tahmin Edilen Duygu: {sentiment_labels[predicted_class]}')
--- model.py
+++ model.py
@@ -4,12 +4,13 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.manifold import TSNE
 import pandas as pd
 
 # Load the data
-df = pd.read_csv('Twitter_Data.csv').dropna(subset=['Metinler', 'Duygular'])
+df = pd.read_csv('Twitter_Datav2.csv').dropna(subset=['Metinler', 'Duygular'])
 
 X = df['Metinler'].values
 y = df['Duygular'].values
@@ -24,10 +25,16 @@ X = pad_sequences(X, maxlen=100)  # Set a fixed sequence length
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
+num_classes = 3  # Number of classes
+
+# One-hot encode the labels
+y_train_one_hot = to_categorical(y_train, num_classes=num_classes)
+y_test_one_hot = to_categorical(y_test, num_classes=num_classes)
+
 # np.savetxt('X_test.csv', X_test, delimiter=',')
 
 # Specify the optimizer and its hyperparameters
-custom_optimizer = Adam(learning_rate=0.001, beta_1=0.9,
+custom_optimizer = Adam(learning_rate=0.0001, beta_1=0.9,
                         beta_2=0.999, epsilon=1e-07, amsgrad=False)
 
 early_stopping = EarlyStopping(
@@ -37,22 +44,22 @@ early_stopping = EarlyStopping(
 model = Sequential([
     Embedding(input_dim=len(tokenizer.word_index) +
               1, output_dim=100, input_length=100),
-    LSTM(64),
-    Dense(1, activation='sigmoid')
+    LSTM(64, dropout=0.2, recurrent_dropout=0.2),
+    Dense(3, activation='softmax')
 ])
 
 if __name__ == '__main__':
     # Compile the model
-    model.compile(optimizer=custom_optimizer, loss='binary_crossentropy',
+    model.compile(optimizer=custom_optimizer, loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     # Train the model
-    model.fit(X_train, y_train, epochs=10, batch_size=32,
-              validation_data=(X_test, y_test), callbacks=[early_stopping])
+    model.fit(X_train, y_train_one_hot, epochs=10, batch_size=32,
+              validation_data=(X_test, y_test_one_hot), callbacks=[early_stopping])
 
     # Evaluate the model
-    test_loss, test_accuracy = model.evaluate(X_test, y_test)
+    test_loss, test_accuracy = model.evaluate(X_test, y_test_one_hot)
     print(f'Test accuracy: {test_accuracy}')
 
     # After training is complete, save the model
-    model.save('model.h5')
+    model.save('model_v3.h5')
